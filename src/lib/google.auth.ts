4d0b3a29fbf86532617487b5f71b3cda42c04360
Henrique Florencio <henrique.florencio@gmail.com>
2026-01-06 13:12:10 -0300

    fix: 503 render

--- src/lib/google.auth.ts
+++ src/lib/google.auth.ts
@@ -6,7 +6,7 @@ export function googleAuth() {
     const jwtClient = new google.auth.JWT(
         config.google.CLIENT_EMAIL,
         undefined,
-        config.google.PRIVATE_KEY,
+        config.google.PRIVATE_KEY?.replace(/\\n/g, '\n'),
         ["https://www.googleapis.com/auth/spreadsheets"],
         undefined,
     )
